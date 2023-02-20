1d77ddf5ae89bbfe87acbf554459fdbe8c2bd7b8
astrononymous <104319212+astrononymous@users.noreply.github.com>
2023-02-20 00:51:28 -0500

    installed express

--- index.js
+++ index.js
@@ -16,16 +16,27 @@ app.use(bodyParser.json());
 // Define a route to handle incoming Telegram webhook requests
 app.post('/telegram-webhook', (req, res) => {
     const message = req.body.message;
+    if (!message) {
+        return res.sendStatus(200);
+    }
     const chatId = message.chat.id;
-    const text = 'Hello, world!';
+    const text = message.text;
+    if (!text) {
+        return res.sendStatus(200);
+    }
 
     // Send a response to the Telegram server to acknowledge receipt of the message
     res.sendStatus(200);
 
+    // If the bot sent the message, don't send a response
+    if (message.from.username === process.env.TELEGRAM_BOT_USERNAME) {
+        return;
+    }
+
     // Send a message to the user who sent the message
     axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`, {
         chat_id: chatId,
-        text: text
+        text: 'Hi'
     });
 });
 
